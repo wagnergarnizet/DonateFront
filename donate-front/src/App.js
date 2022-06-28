28c99a4aa430d2eaf569d49ed4b31883fbf18022
Sara Pereira <sara@gerenteweb.com.br>
2022-06-27 22:08:15 -0300

    navbar and footer

--- donate-front/src/App.js
+++ donate-front/src/App.js
@@ -1,13 +1,16 @@
-import './App.css';
 import * as React from 'react';
 import Rotas from './router';
 import './scss/App.scss';
+import Navbar from './pages/helpers/Navbar';
+import Footer from './pages/helpers/Footer';
 
 function App() {
   return (
     <div className="App">
+      <Navbar></Navbar>
       <h1>Hello World!</h1>
       <Rotas></Rotas>
+      <Footer></Footer>
     </div>
   );
 }
