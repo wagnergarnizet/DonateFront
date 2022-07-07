493a6332163b2a588bc16ea3018a2606fb2c3f1f
Sara Pereira <sara@gerenteweb.com.br>
2022-07-06 22:08:14 -0300

    home banner, sobre nos and campanhas

--- donate-front/src/App.js
+++ donate-front/src/App.js
@@ -9,7 +9,6 @@ function App() {
   return (
     <div className="App">
       <Navbar></Navbar>
-      <h1>Hello World!</h1>
       <Rotas></Rotas>
       <Contato></Contato>
       <Footer></Footer>
