17e975f668edc57fc8989f344b7d370aa565c708
MarmotasJR <matheus.mrodrigues@aluno.faculdadeimpacta.com.br>
2022-08-02 21:09:26 -0300

    Conclusao - Instituicao

--- donate-front/src/pages/helpers/Contato.js
+++ donate-front/src/pages/helpers/Contato.js
@@ -3,13 +3,15 @@ import { NavLink, Link } from 'react-router-dom';
 import BgImage from './../../images/bg-contato.png';
 
 function focusInput(e){
-    var label = document.querySelector('label[htmlFor=' + e.target.id + ']');
-    label.style.top = "-24px";
+    var label = document.querySelector('label[for=' + e.target.id + ']');
+    label.style.top  = "-26px";
+    label.style.left = "0px";
 }
 function blurInput(e){
-    var label = document.querySelector('label[htmlFor=' + e.target.id + ']');
+    var label = document.querySelector('label[for=' + e.target.id + ']');
     if(e.target.value == ""){
         label.style.top = "10px";
+        label.style.left = "15px";
     }
 }
 
