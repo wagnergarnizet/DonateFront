49868ee3010731cbd62c875ef596f3e394e1763a
MarmotasJR <matheus.mrodrigues@aluno.faculdadeimpacta.com.br>
2022-08-03 20:35:30 -0300

    Home - Campanhas

--- donate-front/src/router.js
+++ donate-front/src/router.js
@@ -2,12 +2,13 @@ import * as React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Instituicoes from './pages/Instituicoes';
+import Campanhas from './pages/Campanhas';
 export default function Rotas(){
     return (
         <Routes>
             <Route path="/" element={<Home/>}></Route>
             <Route path="/instituicoes" element={<Instituicoes/>}></Route>
-            <Route path="/instituicoes/:instituicao" element={<Instituicoes/>}></Route>
+            <Route path="/campanhas" element={<Campanhas/>}></Route>
         </Routes>
     );
 }
